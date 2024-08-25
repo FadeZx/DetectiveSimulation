4a6177e6350f562048424a7e02bd29df9927052c
FadeZ <50790210+FadeZx@users.noreply.github.com>
2024-08-25 22:21:57 +0700

    BellSoundCLue Sync with Bell

--- HonkEngine/Engine/Code/GameObjects/BellSoundClueManager.h
+++ HonkEngine/Engine/Code/GameObjects/BellSoundClueManager.h
@@ -8,35 +8,33 @@
 class BellSoundClueManager {
 public:
     BellSoundClueManager(BellSoundClue* leftClue, BellSoundClue* rightClue)
-        : m_leftClue(leftClue), m_rightClue(rightClue) 
+        : m_leftClue(leftClue), m_rightClue(rightClue)
     {
         m_leftClue->setActiveStatus(false);
         m_rightClue->setActiveStatus(false);
     }
 
     void Update(float dt, long frame) {
-        bool isAnyBellRinging = BellManager::GetInstance().isAnyBellRinging();
         bool leftClueActive = false;
         bool rightClueActive = false;
 
-        if (isAnyBellRinging) {
-            float leftBoundary = m_camera.GetLeftBoundary();
-            float rightBoundary = m_camera.GetRightBoundary();
+        float leftBoundary = m_camera.GetLeftBoundary();
+        float rightBoundary = m_camera.GetRightBoundary();
 
-            for (auto* bell : BellManager::GetInstance().GetAllBells()) {
-                if (bell->isBellRinging()) {
-                    glm::vec3 bellPosition = bell->GetPosition();
+        // Iterate through all bells and check if they are ringing
+        for (auto* bell : BellManager::GetInstance().GetAllBells()) {
+            if (bell->isBellRinging()) {
+                glm::vec3 bellPosition = bell->GetPosition();
 
-                    if (bellPosition.x < leftBoundary) { // Bell is outside on the left
-                        m_leftClue->startRinging();
-                        leftClueActive = true;
-                        m_leftClue->setActiveStatus(true);
-                    }
-                    else if (bellPosition.x > rightBoundary) { // Bell is outside on the right
-                        m_rightClue->startRinging();
-                        rightClueActive = true;
-                        m_rightClue->setActiveStatus(true);
-                    }
+                if (bellPosition.x < leftBoundary) { // Bell is outside on the left
+                    m_leftClue->startRinging();
+                    leftClueActive = true;
+                    m_leftClue->setActiveStatus(true);
+                }
+                else if (bellPosition.x > rightBoundary) { // Bell is outside on the right
+                    m_rightClue->startRinging();
+                    rightClueActive = true;
+                    m_rightClue->setActiveStatus(true);
                 }
             }
         }
@@ -61,4 +59,3 @@ private:
     BellSoundClue* m_rightClue;
     Camera& m_camera = Application::GetCamera();  // Add camera reference
 };
-
